efc2ffd28efd1efa5746455478ff6d9c170d2042
Kunal453 <103109790+Kunal453@users.noreply.github.com>
2023-09-19 19:24:24 +0530

    Add files via upload

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -29,3 +29,39 @@ int main()
     return 0;
 }
 
+// void bubbleSort(int arr[], int n)
+// {
+//     // Iterate through the array
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         // Compare adjacent elements
+//         for (int j = 0; j < n - i - 1; j++)<
+//         {
+//             // If the current element is greater than the next element, swap them
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 int temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     // Define the array and its size
+//     int arr[] = {5, 2, 4, 6, 3};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     // Sort the array
+//     bubbleSort(arr, n);
+
+//     // Print the sorted array
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
